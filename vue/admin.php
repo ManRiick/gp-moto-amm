860be6ea30d74a3db1bbc45e54046ba9a4518d32
Lounes <lounes.skr@gmail.com>
2024-10-01 16:30:06 +0200

    lounes

---
+++ vue/admin.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="css/admin.css">
+
+<div class="content">
+    <form action="" method="POST">
+        <input type="password" name="pass" placeholder="mot de passe" required><br>
+        <input type="submit" class="send" value="Se connecter">
+    </form>
+</div>
+
+<?php
+
+function connect(){
+    echo 'ok';
+}
+
+?>
+</div>
